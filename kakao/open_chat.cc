1f4e762e9d5a7c4e029849c06ad0fdcc9975dbb2
Kyle Kwanghyun Jung <khjung828@gmail.com>
2019-06-06 16:05:36 +0900

    [kakao]
    Open chat revisited

--- kakao/open_chat.cc
+++ kakao/open_chat.cc
@@ -4,85 +4,102 @@
 #include <unordered_map>
 #include <algorithm>
 
-const std::string sEntered = "님이 들어왔습니다.";
-const std::string sLeft = "님이 나갔습니다.";
+using namespace std;
 
-std::vector<std::string> solution(std::vector<std::string> record) 
+const string sEntered = "님이 들어왔습니다.";
+const string sLeft = "님이 나갔습니다.";
+
+struct ActionInfo {
+    string action;
+    string id;
+    string name;
+};
+
+vector<string> solution(vector<string> record) 
 {
-    std::vector<std::string> answer;
-    std::string sAnswerEnt;
-    std::string sUid;
-    std::string sNickname;
-    std::string sActionStatus;
-   
-	// map for lasted nick map. Key: user id. Map: Lastest nickname
-    std::unordered_map<std::string, std::string> latestNickMap;
-	// 가장 최근이 뒤 부터니까 뒤에서부터 uId에 대한 최신 nickname을 map에 담는다
-    for (auto rIter = record.rbegin() ; rIter != record.rend() ; ++rIter)
-	{
-		int nFirstSpacePos = (*rIter).find_first_of(" "); // 첫 공백의 위치
-		int nSecondSpacePos = (*rIter).substr(nFirstSpacePos + 1).find_first_of(" "); // 두 번째 공백의 위치
-		std::cout << "First Space Pos : " << nFirstSpacePos << "\n";
-		std::cout << "Second Space Pos : " << nSecondSpacePos << "\n";
+    vector<ActionInfo> infos;
+    vector<string> answer;
+    unordered_map<string, string> nameMap; // map from uid to recent nickname.
 
-		// 두 번째 공백이 없다면 leave
-		if (nSecondSpacePos == std::string::npos)
-		{
-			sUid = (*rIter).substr(nFirstSpacePos + 1);
-			sNickname = latestNickMap.find(sUid)->second;
-			sAnswerEnt = sNickname + sLeft;
-			answer.emplace_back(sAnswerEnt);
-			break;
-		}
-		else
-		{
-			sUid = (*rIter).substr(nFirstSpacePos + 1, nSecondSpacePos);
-			sActionStatus = (*rIter).substr(0, nFirstSpacePos);
-			auto nickIter = latestNickMap.find(sUid);	
+    for(size_t i = 0 ; i < record.size() ; ++i)
+    {
+        ActionInfo info;
+        string rec = record[i];
+        cout << "Current record : " << rec << endl;
+        
+        size_t firstSpacePos = rec.find_first_of(" ");
+        info.action = rec.substr(0, firstSpacePos);
 
-			if (nickIter == latestNickMap.end())
-			{
-				sNickname = (*rIter).substr(nFirstSpacePos + nSecondSpacePos + 2);
-				latestNickMap.insert(std::pair<std::string, std::string>(sUid, sNickname));
-			}
-			else
-			{
-				sNickname = nickIter->second; 
-			}
+        cout << "Current action : " << info.action << endl; 
 
-			if (sActionStatus.compare("Enter") == 0)
-			{
-				sAnswerEnt = sNickname + sEntered;
-				answer.emplace_back(sAnswerEnt);
-			}
-			else
-			{
-				latestNickMap[sUid] = sNickname;
-			}
-		}
+        size_t secondSpacePos = rec.find_first_of(" ", firstSpacePos + 1);
 
-	}
+        // 2번째 공백이 없는 경우는, leave
+        if(secondSpacePos == string::npos)
+        {
+            info.id = rec.substr(firstSpacePos + 1);
+            cout << "Current id : " << info.id << endl;
+            auto it = nameMap.find(info.id);
+            info.name = it->second;
+        }
+        // 2번째 공백이 있다면, Enter/Change
+        else
+        {
+            info.id = rec.substr(firstSpacePos + 1, secondSpacePos - firstSpacePos - 1);
+            cout << "Current id : " << info.id << endl;
 
-	std::reverse(std::begin(answer), std::end(answer));
+            info.name = rec.substr(secondSpacePos + 1);
+            cout << "Current name : " << info.name << endl;
 
-	return answer; 
+            // Enter
+            if(info.action.compare("Enter") == 0)
+            {
+                if(nameMap.find(info.id) != nameMap.end())
+                    nameMap[info.id] = info.name;
+                else
+                    nameMap.insert(make_pair(info.id, info.name));
+            }
+            // Change name
+            else
+            {
+                nameMap[info.id] = info.name;
+            }
+        }
+        infos.emplace_back(info);
+    }
+
+    for(size_t i = 0 ; i < infos.size() ; ++i)
+    {
+        if(infos[i].action.compare("Enter") == 0)
+        {
+            answer.emplace_back(nameMap.find(infos[i].id)->second + sEntered);
+        }
+        else if(infos[i].action.compare("Leave") == 0)
+        {
+            answer.emplace_back(nameMap.find(infos[i].id)->second + sLeft);
+        }
+    }
+
+    return answer; 
 }
 
 int main (int argc, char* argv[])
 { 
-	std::vector<std::string> input = 
-	{"Enter uid1234 Muzi",
-		"Enter uid4567 Prodo",
-		"Leave uid1234", 
-		"Enter uid1234 Prodo", 
-		"Change uid4567 Ryan"};
+    vector<string> input = 
+    {
+        "Enter uid1234 Muzi",
+        "Enter uid4567 Prodo",
+        "Leave uid1234", 
+        "Enter uid1234 Prodo", 
+        "Change uid4567 Ryan",
+    };
 
-	std::vector<std::string> output = solution(input);
+    vector<string> output = solution(input);
 
-	std::cout << "---Printing out the Output---" << std::endl;
-	for (auto ent : output)
-	{
-		std::cout << ent << "\n";
-	}
+    cout << "---Printing out the Output---" << endl;
+    for (auto ent : output)
+    {
+        cout << ent << "\n";
+    }
 }
 
